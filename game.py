269c221a565cb909ac1d5450a0f65f3276fba8c2
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 16:33:27 -0700

    limit number of guesses

--- game.py
+++ game.py
@@ -2,20 +2,25 @@
 
 import random
 
-best_score = None
 player_name = raw_input('Howdy, what\'s your name?\n(type in your name) ')
 scores = []
 
-def main_game():
-    secret_number = random.randint(1, 100)
-    print "I'm thinking of a number between 1 and 100.\nTry to guess my number."
 
-    guess_count = 1
+def main_game():
+    turn_limit = limit_tries()
+    secret_number = random.randint(1, 100)
+    print "I'm thinking of a number between 1 and 100. Try to guess my number."
+
+    guess_count = 0
 
     while True:
+        if turn_limit is not None:
+            turns_left = turn_limit - guess_count
+            print 'You have %d tries left.' % turns_left
         player_guess = raw_input('Your guess? ')
         try:
             player_guess = int(float(player_guess))
+            guess_count += 1
         except ValueError:
             print 'That\'s not a number!'
             continue
@@ -23,7 +28,10 @@ def main_game():
             print 'That isn\'t a number between 1 and 100!'
             continue
         if player_guess != secret_number:
-            guess_count += 1
+            if turn_limit is not None:
+                if guess_count == turn_limit:
+                    print 'Too many tries! The number was %d.' % (secret_number)
+                    break
             if player_guess < secret_number:
                 print 'Your guess is too low, try again.'
             elif player_guess > secret_number:
@@ -32,11 +40,35 @@ def main_game():
             scores.append(guess_count)
             print 'Well done, %s! You found my number in %d tries.' % \
                 (player_name, guess_count)
-            print 'Your best score is {}.'.format(min(scores))
-            play_again = raw_input("Press Y to play again: ")
-            if play_again == "Y" or play_again == "y":
+            break
+
+    end_game()
+
+
+def end_game():
+    try:
+        print 'Your best score is {}.'.format(min(scores))
+    except ValueError:
+        pass
+    play_again = raw_input("Press Y to play again: ")
+    if play_again.upper() == "Y":
+        main_game()
+    else:
+        exit()
+
+
+def limit_tries():
+    limit_prompt = raw_input('Press Y for a limit on your guesses: ')
+    if limit_prompt.upper() == 'Y':
+        while True:
+            max_tries = raw_input('Enter a max number of tries: ')
+            try:
+                max_tries = int(float(max_tries))
+                return max_tries
+            except ValueError:
+                print 'That\'s not a number!'
                 continue
-            else:
-                exit()
+    else:
+        return None
 
 main_game()
