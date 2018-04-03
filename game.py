d894746775a49555f55ba354b992e4562dbd7ffb
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-03 16:00:18 -0700

    High score and play again

--- game.py
+++ game.py
@@ -2,29 +2,41 @@
 
 import random
 
+best_score = None
 player_name = raw_input('Howdy, what\'s your name?\n(type in your name) ')
-secret_number = random.randint(1, 100)
-print "I'm thinking of a number between 1 and 100.\nTry to guess my number."
+scores = []
 
-guess_count = 1
+def main_game():
+    secret_number = random.randint(1, 100)
+    print "I'm thinking of a number between 1 and 100.\nTry to guess my number."
 
-while True:
-    player_guess = raw_input('Your guess? ')
-    try:
-        player_guess = int(float(player_guess))
-    except ValueError:
-        print 'That\'s not a number!'
-        continue
-    if player_guess < 1 or player_guess > 100:
-        print 'That isn\'t a number between 1 and 100!'
-        continue
-    if player_guess != secret_number:
-        guess_count += 1
-        if player_guess < secret_number:
-            print 'Your guess is too low, try again.'
-        elif player_guess > secret_number:
-            print 'Your guess is too high, try again.'
-    else:
-        print 'Well done, %s! You found my number in %d tries.' % \
-            (player_name, guess_count)
-        break
+    guess_count = 1
+
+    while True:
+        player_guess = raw_input('Your guess? ')
+        try:
+            player_guess = int(float(player_guess))
+        except ValueError:
+            print 'That\'s not a number!'
+            continue
+        if player_guess < 1 or player_guess > 100:
+            print 'That isn\'t a number between 1 and 100!'
+            continue
+        if player_guess != secret_number:
+            guess_count += 1
+            if player_guess < secret_number:
+                print 'Your guess is too low, try again.'
+            elif player_guess > secret_number:
+                print 'Your guess is too high, try again.'
+        else:
+            scores.append(guess_count)
+            print 'Well done, %s! You found my number in %d tries.' % \
+                (player_name, guess_count)
+            print 'Your best score is {}.'.format(min(scores))
+            play_again = raw_input("Press Y to play again: ")
+            if play_again == "Y" or play_again == "y":
+                continue
+            else:
+                exit()
+
+main_game()
